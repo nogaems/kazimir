d88c4a1ac2403441827c4fad7356c02f1b956247
nogaems <nogaems@users.noreply.github.com>
2018-04-04 17:39:16 +0300

    Add rendering

--- draw.py
+++ draw.py
@@ -3,7 +3,9 @@
 
 import imp
 import itertools
+import math
 import os
+from PIL import Image, ImageDraw, ImageFont
 
 
 class SecureError(Exception):
@@ -16,6 +18,7 @@ class IntegrityError(Exception):
 
 class Artist:
     layout = None
+    font = None
     # There's the default configuration
     config = {
         'bg': (255, 255, 255, 255),
@@ -27,11 +30,13 @@ class Artist:
         'cell_bg': (102, 102, 102, 255),
         'cell_fg': (241, 194, 50, 255)
     }
+    spacing = round(config['cell_size'] * 0.1)  # 10% of any cell size
 
     def __init__(self, name, config='config.py'):
         self.load_layout(name)
         self.normalize_optionals()
         self.load_config()
+        self.load_font()
 
     def safe_load(self, path):
         code = open(path).read()
@@ -60,6 +65,12 @@ class Artist:
             if option in config:
                 self.config[option] = config[option]
 
+    def load_font(self):
+        self.font = ImageFont.truetype(
+            font=self.config['font'],
+            size=self.config['font_size']
+        )
+
     # TODO: configuration validation
 
     @staticmethod
@@ -69,7 +80,7 @@ class Artist:
 
     def iscorrect(self, code):
         '''
-        Checks the only required fields. Returns a tuple with a result and a reason. 
+        Checks the only required fields. Returns a tuple with a result and a reason.
         If the result is True, the reason is None.
         Since I don't have macro in python this code will contain a lot of boilerplate.
         I'm considering any suggestions how to improve this function.
@@ -119,9 +130,93 @@ class Artist:
             if not isinstance(obj, str):
                 setattr(self.layout, entry, str(obj))
 
+    def draw_chord(self, chord, width, height):
+        name = list(chord.keys())[0]
+        value = list(chord.values())[0]
+        cols = len(value[0])
+        rows = len(value)
+        name_w, name_h = self.font.getsize(name)
+        img = Image.new('RGBA', (width, height), self.config['bg'])
+        draw = ImageDraw.Draw(img)
+        draw.text((width / 2 - name_w / 2, self.spacing), name,
+                  font=self.font, fill=self.config['font_color'])
+        cursor = [width / 2 - (cols / 2) * self.config['cell_size'] - self.spacing * (cols - 1),
+                  self.spacing * 2 + name_h]
+        for row in value:
+            for cell in row:
+                if cell is None:
+                    continue
+                draw.rectangle(
+                    [cursor[0],
+                     cursor[1],
+                     cursor[0] + self.config['cell_size'],
+                     cursor[1] + self.config['cell_size']],
+                    fill=self.config['cell_bg'],
+                    outline=(0, 0, 0, 255)
+                )
+                if int(cell) == 1:
+                    draw.ellipse(
+                        [cursor[0] + self.spacing / 2,
+                         cursor[1] + self.spacing / 2,
+                         cursor[0] + self.config['cell_size'] -
+                         self.spacing / 2,
+                         cursor[1] + self.config['cell_size'] -
+                         self.spacing / 2],
+                        fill=self.config['cell_fg'],
+                        outline=(0, 0, 0, 255)
+                    )
+                cursor[0] += self.spacing + self.config['cell_size']
+            cursor[0] = width / 2 - \
+                (cols / 2) * self.config['cell_size'] - \
+                self.spacing * (cols - 1)
+            cursor[1] += self.spacing + self.config['cell_size']
+        return img
+
+    def draw(self):
+        max_len_name = max([list(c.keys())[0] for c in self.layout.data],
+                           key=lambda x: len(x))
+        name_w, name_h = self.font.getsize(max_len_name)
+        chords = []
+        cols = len(list(self.layout.data[0].values())[0][0])
+        rows = len(list(self.layout.data[0].values())[0])
+        width = cols * self.config['cell_size'] + self.spacing * (cols + 1)
+        width = width if width >= name_w else name_w + self.spacing * 2
+        height = rows * self.config['cell_size'] + \
+            self.spacing * (rows + 1) + name_h + self.spacing
+        for chord in self.layout.data:
+            chords.append(self.draw_chord(chord, width, height))
+        desc_w, desc_h = self.font.getsize(self.layout.desc)
+        result_w = self.config['cols'] * width
+        result_h = math.ceil(len(chords) / self.config['cols']) * height + \
+            self.spacing * 2 + \
+            desc_h
+        result = Image.new('RGBA', (result_w, result_h), self.config['bg'])
+        draw = ImageDraw.Draw(result)
+        draw.text((result_w / 2 - desc_w / 2, self.spacing),
+                  self.layout.desc,
+                  fill=self.config['font_color'],
+                  font=self.font
+                  )
+        counter = 0
+        cursor = [0, desc_h + self.spacing * 2]
+        for chord in chords:
+            counter += 1
+            result.paste(chord, tuple(cursor))
+            if counter // self.config['cols'] != 1:
+                cursor[0] += width
+            else:
+                cursor[0] = 0
+                cursor[1] += height
+                counter = 0
+
+        return result
+
 
 if __name__ == '__main__':
     try:
-        a = Artist('example.py')
+        a = Artist('example2.py')
+        r = a.draw()
+        r.save('chords.png')
+
     except Exception as e:
         print("An error has occured\n{}: {}".format(e.__class__.__name__, e))
